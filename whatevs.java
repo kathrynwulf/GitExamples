c991dc31865d8d50df2e8b6325ee52bdda907663
kathrynwulf <59715436+kathrynwulf@users.noreply.github.com>
2020-01-10 00:07:51 -0500

    Even better print message (#2)
    * wub you
    
    * Add peeping message

--- whatevs.java
+++ whatevs.java
@@ -3,6 +3,11 @@ public class whatevs{
         System.out.println("Hello Babies!");
         System.out.println("Babies!");
         System.out.println("I wub you!");
+
+        System.out.println("I wub you too!");
+        System.out.println("Stop peeping in my shiz, boy!");
+
         System.out.println("I'm a sneaky message");
+
     }
 }
